fc94529f6545b74737331f61af77d8f7bf7fe09c
punkername007 <152019449+punkername007@users.noreply.github.com>
2023-12-18 17:40:21 +0100

    Added JavaScript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Generating a poem ... please wait",
+    autoStart: true,
+    cursor: null,
+    delay: 80,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-form-element");
+poemFormElement.addEventListener("submit", generatePoem);
