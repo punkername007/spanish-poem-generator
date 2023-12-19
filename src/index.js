93cf9f19025440ce3c3b4ba5af75a2adcce09955
punkername007 <152019449+punkername007@users.noreply.github.com>
2023-12-19 01:23:52 +0100

    Linked AI API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: null,
+    delay: 1,
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  new Typewriter("#poem", {
-    strings: "Generating a poem ... please wait",
-    autoStart: true,
-    cursor: null,
-    delay: 80,
-  });
+  let userInstruction = document.querySelector("#instructions");
+  let apiKey = "e2c0b68bt1bfbc04o7da0f6ea7720334";
+  let context =
+    "Your are an experienced and passionate poet who writes short poems. Your mission is to provide a five line poem in basic HTML following the user instructions. You should not write a title and a signature should be written at the bottom of the poem. ONLY the signature should be written in the following format: <strong>'She Codes AI'</strong>.";
+  let prompt = `User instructions: Generate a structured poem in spanish about ${userInstruction.value}`;
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-form-element");
